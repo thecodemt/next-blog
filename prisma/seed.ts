eaebc11c5ec9abf59b767efa3eed83312b6962b3
john <3097498625@qq.com>
2026-03-15 17:02:29 +0800

    Correctly configure Prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({
+  adapter,
+});
 
 async function main() {
   // Create default categories
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL, 
+}); 
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export default prisma; 
